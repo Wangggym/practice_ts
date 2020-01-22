8be9d510368fa02a1b84693ce7b45df0f91b6278
wangym <908289704@qq.com>
2020-01-22 13:57:11 +0800

    feat: function

--- function/selectable_default_params.ts
+++ function/selectable_default_params.ts
@@ -1,8 +1,9 @@
 // 可选和默认参数
-function buildName(firstName: string, lastName: string) {
+function buildName(firstName: string, lastName: string = "Smith") {
     return firstName + " " + lastName;
 }
 
 let result1 = buildName('Bob');
 let result2 = buildName('Bob', 'Adams', 'Sr.');
 let result3 = buildName('Bob', 'Adams');
+
